8849381e3e4972fce54c5f095a5441074d4cbd7e
Iuri Freire <ifreire@noreply.com>
2017-11-13 20:39:04 -0300

    Alterando para Facade

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,41 +6,63 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		MySingleton sing001 = MySingleton.Instance();
-		System.out.println("sing001: ");
+		MySingletonFAC.Instance().sum();
+		MySingletonFAC.Instance().sub();
+		MySingletonFAC.Instance().mult();
+		MySingletonFAC.Instance().div();
+		
+		MySingletonFAC.Instance().setXY(10, 5);
+		
+		MySingletonFAC.Instance().sum();
+		MySingletonFAC.Instance().sub();
+		MySingletonFAC.Instance().mult();
+		MySingletonFAC.Instance().div();
+		
+		IMySingletonFAC sing001 = MySingletonFAC.Instance();
+		
 		sing001.sum();
-		System.out.println("");
-		System.out.println("");
+		sing001.sub();
+		sing001.mult();
+		sing001.div();
+		
+		System.out.print((8 + 8 + 9 + 8.5 + 9 + 10 + 9.5 + 9)/8);
 		
 		
-		MySingleton sing002 = MySingleton.Instance();
-		System.out.println("sing002: ");
-		sing002.sub();
-		sing002.setXY(100, 3);
-		System.out.println("");
-		System.out.println("");
-		
-		
-		MySingleton sing003 = MySingleton.Instance();
-		System.out.println("sing003: ");
-		sing003.mult();
-		System.out.println("");
-		System.out.println("");
-		
-		
-		MySingleton sing004 = MySingleton.Instance();
-		System.out.println("sing004: ");
-		sing004.div();
-		sing004.setXY(20, 30);
-		System.out.println("");
-		System.out.println("");
-		
-		
-		MySingleton sing005 = MySingleton.Instance();
-		System.out.println("sing005: ");
-		sing005.sum();
-		
-		
-		//MathController mc = new MathController();
+//		IMySingletonFAC sing001 = MySingletonFAC.Instance();
+//		System.out.println("sing001: ");
+//		sing001.sum();
+//		System.out.println("");
+//		System.out.println("");
+//		
+//		
+//		IMySingletonFAC sing002 = MySingletonFAC.Instance();
+//		System.out.println("sing002: ");
+//		sing002.sub();
+//		sing002.setXY(100, 3);
+//		System.out.println("");
+//		System.out.println("");
+//		
+//		
+//		IMySingletonFAC sing003 = MySingletonFAC.Instance();
+//		System.out.println("sing003: ");
+//		sing003.mult();
+//		System.out.println("");
+//		System.out.println("");
+//		
+//		
+//		IMySingletonFAC sing004 = MySingletonFAC.Instance();
+//		System.out.println("sing004: ");
+//		sing004.div();
+//		sing004.setXY(20, 30);
+//		System.out.println("");
+//		System.out.println("");
+//		
+//		
+//		IMySingletonFAC sing005 = MySingletonFAC.Instance();
+//		System.out.println("sing005: ");
+//		sing005.sum();
+//		
+//		
+//		//MathController mc = new MathController();
 	}
 }
